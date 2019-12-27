5b53c501a996b83d68ff87f28c6671837fad90cb
Semjon Wilhelm <semjon-wilhelm@hotmail.de>
2019-12-27 14:17:42 +0100

    Add Go HTTP example
    Please see readme.md for more information

---
+++ snippets/goHttpExample/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type App struct {
+	Router *mux.Router
+}
+
+type HelloWorldResponse struct {
+	Message       string `json:"msg"`
+	ProvidedName  string `json:"name"`
+}
+
+func HelloWorldGetHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		name := r.FormValue("name")
+
+		response := HelloWorldResponse{
+			ProvidedName: name,
+			Message: fmt.Sprintf("Hello, %s!", name),
+		}
+
+		JSONResponse(w, 200, response)
+	})
+}
+
+func HelloWorldHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+
+		response := HelloWorldResponse{
+			ProvidedName: vars["name"],
+			Message: fmt.Sprintf("Hello, %s!", vars["name"]),
+		}
+
+		JSONResponse(w, 200, response)
+	})
+}
+
+func JSONResponse(w http.ResponseWriter, code int, output interface{}) {
+	// Convert our interface to JSON
+	response, _ := json.Marshal(output)
+	// Set the content type to json for browsers
+	w.Header().Set("Content-Type", "application/json")
+	// Our response code
+	w.WriteHeader(code)
+
+	w.Write(response)
+}
+
+func (a *App) Initialize() {
+	router := mux.NewRouter()
+	router.Handle("/hello", HelloWorldGetHandler() ).Methods("GET")
+
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	a.Router = router
+	
+	router.Handle("/hello/{name}", HelloWorldHandler())
+}
+
+func (a *App) Run(addr string) {
+	http.ListenAndServe(addr, a.Router)
+}
+
+func main() {
+	a := App{}
+	a.Initialize()
+	a.Run(":8080")
+}
---
+++ snippets/goHttpExample/readme.md
@@ -0,0 +1,18 @@
+# Go HTTP Example
+
+This go example uses mux of the gorilla webkit.
+
+## Set Up
+
+ - execute `go mod download`
+
+## Run
+
+ - verify that no other process runs on port 8080
+ - execute `go run .`
+
+## Try
+
+ - http://127.0.0.1:8080/hello?name=Studi%20Guide
+ - http://127.0.0.1:8080/hello/Studi%20Guide
+ - http://127.0.0.1:8080/static/style.css
