51ba63373307eeb5be19e4fdaaa3666b957050b7
sharpSteff <sharp.kneidl@gmail.com>
2020-01-18 16:57:14 +0100

    add additional logs on startup procedure

--- snippets/goHttpExample/src/cmd/main.go
+++ snippets/goHttpExample/src/cmd/main.go
@@ -4,19 +4,26 @@ import (
 	"github.com/gorilla/mux"
 	"httpExample/pkg/roomcontroller"
 	"httpExample/pkg/shoppinglist"
+	"log"
 	"net/http"
 )
 
 func Main() (error) {
-
+	log.Print("Starting initializing main controllers ...")
 	router := mux.NewRouter()
+
+	log.Print("Creating shopping list")
 	a := shoppinglist.ShoppingList{}
 	a.Initialize(router)
+
+	log.Print("Creating room controller")
 	roomController := roomcontroller.RoomController{}
 	roomController.Initialize(router)
 	//a.Run(":8080")
 
-	http.ListenAndServe(":8080", router)
+	port := ":8080"
+	log.Printf("Starting http listener on %s", port)
+	http.ListenAndServe(port, router)
 
 	return nil
 
