a1b2652acc32b330be423b5a5289c1f6102e0224
Semjon Wilhelm <semjon-wilhelm@hotmail.de>
2020-03-06 15:12:35 +0100

    STDG-77 Add DistanceTo other Coordinate method

--- back/pkg/navigation/coordinate.go
+++ back/pkg/navigation/coordinate.go
@@ -1,7 +1,20 @@
 package navigation
 
+import (
+	"math"
+)
+
 type Coordinate struct {
 	X int64 `json:"x" xml:"x" db:"X"`
 	Y int64 `json:"y" xml:"y" db:"Y"`
 	Z int64 `json:"z" xml:"z" db:"Z"`
+}
+
+func (c Coordinate) DistanceTo(other Coordinate) (int64) {
+
+	p := Coordinate{X: c.X - other.X, Y: c.Y - other.Y, Z: c.Z - other.Z}
+
+	distance := math.Sqrt(math.Pow(float64(p.X), 2) + math.Pow(float64(p.Y), 2) + math.Pow(float64(p.Z), 2))
+
+	return int64(math.Round(distance))
 }
---
+++ back/pkg/navigation/coordinate_test.go
@@ -0,0 +1,22 @@
+package navigation
+
+import "testing"
+
+func TestDistanceTo(t *testing.T) {
+	O := Coordinate{X: 0, Y: 0, Z: 0}
+	p1 := Coordinate{X: 1, Y: 2, Z: 3}
+
+	if distance := O.DistanceTo(p1); distance != 4 {
+		t.Error("expected: 4, got: ", distance)
+	}
+
+	if distance := p1.DistanceTo(O); distance != 4 {
+		t.Error("expected: 4, got: ", distance)
+	}
+
+	p2 := Coordinate{X: 3, Y: 4, Z: 5}
+	if distance := p2.DistanceTo(p1); distance != 3 {
+		t.Error("expected: 3, got: ", distance)
+	}
+
+}
