5b5d106c9d322ec26b6f40158c64d959dcf68d9f
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-03-12 21:54:05 +0100

    Implement Dijkstra algorithm
    STDG-78
    
    Implement Dijkstra Algorithm and Tests using PathNodes.
     - Dijkstra takes PathNodes array
     - Returns array of PathNodes, ordered by the path
     - Add Distance as return value as int64

--- back/pkg/navigation/dijkstra.go
+++ back/pkg/navigation/dijkstra.go
@@ -3,14 +3,46 @@ package navigation
 import "github.com/RyanCarrier/dijkstra"
 
 type DijkstraNavigation struct {
-	graph dijkstra.Graph
-	pathNodes []PathNode
+	graph *dijkstra.Graph
+	pathNodes map[int]PathNode
 }
 
 func NewDijkstraNavigation(pathNodes []PathNode) (*DijkstraNavigation) {
-	return &DijkstraNavigation{pathNodes:pathNodes}
+
+	d := DijkstraNavigation{pathNodes: make(map[int]PathNode), graph: dijkstra.NewGraph()}
+
+	// Add vertices and fill map
+	for _, node := range pathNodes {
+		d.pathNodes[node.Id] = node
+		d.graph.AddVertex(node.Id)
+	}
+
+	// Add arcs
+	for _, source := range pathNodes {
+		for _, dest := range source.ConnectedNodes {
+			d.graph.AddArc(source.Id, dest.Id, int64(source.Coordinate.DistanceTo(dest.Coordinate)))
+		}
+	}
+
+	return &d
 }
 
-func (d *DijkstraNavigation) GetRoute(start, end PathNode) ([]PathNode) {
-	return []PathNode{}
+func (d *DijkstraNavigation) GetRoute(start, end PathNode) (path []PathNode, distance int64, err error) {
+
+	if start.Coordinate.Equals(end.Coordinate) {
+		return nil, 0, nil
+	}
+
+	bestPath, err := d.graph.Shortest(start.Id, end.Id)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	pathNodes := make([]PathNode, len(bestPath.Path))
+	for i, id := range bestPath.Path {
+		pathNodes[i] = d.pathNodes[id]
+	}
+
+	return pathNodes, bestPath.Distance, nil
 }
--- back/pkg/navigation/dijkstra_test.go
+++ back/pkg/navigation/dijkstra_test.go
@@ -1,7 +1,7 @@
 package navigation
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -13,29 +13,105 @@ func TestDijkstraNavigation_GetRoute1(t *testing.T) {
 	var pathNodes []PathNode
 
 	// P0(1,2,3) -> P1(3,2,3) -> P2(3,4,3)
-	//           -> P3(1,4,3) ->
+	//          `-> P3(1,4,3) -^
 	pathNodes = append(pathNodes, PathNode{Id: 0, Coordinate: Coordinate{X: 1, Y: 2, Z: 3,}, Group: nil, ConnectedNodes: nil })
 	pathNodes = append(pathNodes, PathNode{Id: 1, Coordinate: Coordinate{X: 3, Y: 2, Z: 3,}, Group: nil, ConnectedNodes: nil })
 	pathNodes = append(pathNodes, PathNode{Id: 2, Coordinate: Coordinate{X: 3, Y: 4, Z: 3,}, Group: nil, ConnectedNodes: nil })
 	pathNodes = append(pathNodes, PathNode{Id: 3, Coordinate: Coordinate{X: 1, Y: 4, Z: 3,}, Group: nil, ConnectedNodes: nil })
 
-	pathNodes[0].ConnectedNodes = []*PathNode{&pathNodes[1]}
+	pathNodes[0].ConnectedNodes = []*PathNode{&pathNodes[1], &pathNodes[3]}
 	pathNodes[1].ConnectedNodes = []*PathNode{&pathNodes[0], &pathNodes[2]}
-	pathNodes[2].ConnectedNodes = []*PathNode{&pathNodes[1]}
-
+	pathNodes[2].ConnectedNodes = []*PathNode{&pathNodes[1], &pathNodes[3]}
 	pathNodes[3].ConnectedNodes = []*PathNode{&pathNodes[0], &pathNodes[2]}
 
-	navigation := NewDijkstraNavigation(pathNodes)
+	expected1 := []PathNode{pathNodes[0], pathNodes[1], pathNodes[2]}
+	expected2 := []PathNode{pathNodes[0], pathNodes[3], pathNodes[2]}
 
-	route := navigation.GetRoute(pathNodes[0], pathNodes[2])
-	fmt.Println(route)
+	navigation := NewDijkstraNavigation(pathNodes)
+	route, distance, err := navigation.GetRoute(pathNodes[0], pathNodes[2])
+
+	if distance != 4  {
+		t.Error("expected distance:", 0, "; got:", distance)
+	}
+
+	if err != nil {
+		t.Error("expected nil, got:", err)
+	}
+
+	if !reflect.DeepEqual(expected1, route) && !reflect.DeepEqual(expected2, route) {
+		t.Error("expected route", expected1, "or", expected2, "; got: ", route)
+	}
 
 }
 
 func TestDijkstraNavigation_GetRoute2(t *testing.T) {
 
+	var pathNodes []PathNode
+
+	// P0(1,2,3) -> P1(8,2,3) -> P2(8,10,3) ----------------> P3(8,10,6)
+	//          `-> P4(10,2,3)-> P5(10,2,6) -> P6(10,10,6) -^
+	pathNodes = append(pathNodes, PathNode{Id: 0, Coordinate: Coordinate{X: 1, Y: 2, Z: 3}, Group: nil, ConnectedNodes: nil})
+	pathNodes = append(pathNodes, PathNode{Id: 1, Coordinate: Coordinate{X: 8, Y: 2, Z: 3}, Group: nil, ConnectedNodes: nil})
+	pathNodes = append(pathNodes, PathNode{Id: 2, Coordinate: Coordinate{X: 8, Y: 10, Z: 3}, Group: nil, ConnectedNodes: nil})
+	pathNodes = append(pathNodes, PathNode{Id: 3, Coordinate: Coordinate{X: 8, Y: 10, Z: 6}, Group: nil, ConnectedNodes: nil})
+	pathNodes = append(pathNodes, PathNode{Id: 4, Coordinate: Coordinate{X: 10, Y: 2, Z: 3}, Group: nil, ConnectedNodes: nil})
+	pathNodes = append(pathNodes, PathNode{Id: 5, Coordinate: Coordinate{X: 10, Y: 2, Z: 6}, Group: nil, ConnectedNodes: nil})
+	pathNodes = append(pathNodes, PathNode{Id: 6, Coordinate: Coordinate{X: 10, Y: 10, Z: 6}, Group: nil, ConnectedNodes: nil})
+
+	pathNodes[0].ConnectedNodes = []*PathNode{&pathNodes[1], &pathNodes[4]}
+	pathNodes[1].ConnectedNodes = []*PathNode{&pathNodes[0], &pathNodes[2]}
+	pathNodes[2].ConnectedNodes = []*PathNode{&pathNodes[1], &pathNodes[3]}
+	pathNodes[3].ConnectedNodes = []*PathNode{&pathNodes[2], &pathNodes[6]}
+	pathNodes[4].ConnectedNodes = []*PathNode{&pathNodes[0], &pathNodes[5]}
+	pathNodes[5].ConnectedNodes = []*PathNode{&pathNodes[4], &pathNodes[6]}
+	pathNodes[6].ConnectedNodes = []*PathNode{&pathNodes[5], &pathNodes[3]}
+
+	expected := []PathNode{pathNodes[0], pathNodes[1], pathNodes[2], pathNodes[3]}
+
+	navigation := NewDijkstraNavigation(pathNodes)
+	route, distance, err := navigation.GetRoute(pathNodes[0], pathNodes[3])
+
+	if distance != 18  {
+		t.Error("expected distance:", 0, "; got:", distance)
+	}
+
+	if err != nil {
+		t.Error("expected nil, got:", err)
+	}
+
+	if len(route) != len(expected) {
+		t.Error("expected route len: ", len(expected), "; got:", len(route))
+	}
+
+	for i, _ := range route {
+		if expected[i].Id != route[i].Id {
+			t.Error("expected Id:", expected[i].Id, "; got:", route[i].Id)
+		}
+	}
+
 }
 
 func TestDijkstraNavigation_GetRoute3(t *testing.T) {
+
+	var pathNodes []PathNode
+
+	// P1(3,3,3) -> P2(3,3,3)
+	pathNodes = append(pathNodes, PathNode{Id: 0, Coordinate: Coordinate{X: 3, Y: 3, Z: 3}, Group: nil, ConnectedNodes: nil})
+	pathNodes = append(pathNodes, PathNode{Id: 1, Coordinate: Coordinate{X: 3, Y: 3, Z: 3}, Group: nil, ConnectedNodes: nil})
+
+	pathNodes[0].ConnectedNodes = []*PathNode{&pathNodes[1]}
+	pathNodes[1].ConnectedNodes = []*PathNode{&pathNodes[0]}
+
+	navigation := NewDijkstraNavigation(pathNodes)
+
+	route, distance, err := navigation.GetRoute(pathNodes[0], pathNodes[1])
+
+	if distance != 0  {
+		t.Error("expected distance:", 0, "; got:", distance)
+	}
+
+	if err != nil && route != nil {
+		t.Error("expected nil, got:", err, route)
+	}
 
 }
