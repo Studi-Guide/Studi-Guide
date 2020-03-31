58ab478b9110ceb20fbf50cae916571b51d05581
sharpSteff <sharp.kneidl@gmail.com>
2020-03-31 16:08:41 +0200

    updated xml doc for swagger

--- back/pkg/map/mapcontroller.go
+++ back/pkg/map/mapcontroller.go
@@ -16,10 +16,18 @@ type MapController struct {
 func NewMapController(router *gin.RouterGroup, provider models.RoomServiceProvider) error {
 	r := MapController{router: router, provider: provider}
 	r.router.GET("/", r.GetMapItems)
-	r.router.GET("/:floor", r.GetMapItemsFromFloor)
+	r.router.GET("/floor", r.GetMapItemsFromFloor)
 	return nil
 }
 
+// GetMapItems godoc
+// @Summary Get All Map Items
+// @Description Gets all map items of available rooms and connector spaces (corridor, stairs, etc..)
+// @Accept  json
+// @Produce  json
+// @Tags MapController
+// @Success 200 {array} models.MapItem
+// @Router /map [get]
 func (l MapController) GetMapItems(c *gin.Context) {
 	rooms, err := l.provider.GetAllRooms()
 	if err != nil {
@@ -42,7 +50,15 @@ func (l MapController) GetMapItems(c *gin.Context) {
 	l.CreateAndSendMapList(rooms, connectors, c)
 }
 
-
+// GetMapItems godoc
+// @Summary Get All Map Items
+// @Description Gets all map items of available rooms and connector spaces (corridor, stairs, etc..)
+// @Accept  json
+// @Produce  json
+// @Tags MapController
+// @Param floor query int false "filter map items by floor"
+// @Success 200 {array} models.MapItem
+// @Router /map/floor [get]
 func (l MapController) GetMapItemsFromFloor(c *gin.Context) {
 	floor := c.Query("floor") //mux.Vars(r)["name"]
 
