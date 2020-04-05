74439d9fb1a91bb82f6c8e0477a8d428d7ae228f
sharpSteff <sharp.kneidl@gmail.com>
2020-04-05 16:32:50 +0200

    Added FilterConnectorSpaces to roomcontroller and changed the mapcontroller call to use these API

--- back/pkg/map/mapcontroller.go
+++ back/pkg/map/mapcontroller.go
@@ -22,29 +22,67 @@ func NewMapController(router *gin.RouterGroup, provider models.RoomServiceProvid
 
 // GetMapItems godoc
 // @Summary Get All Map Items
-// @Description Gets all map items of available rooms and connector spaces (corridor, stairs, etc..)
+// @Description Gets map items of available rooms and connector spaces (corridor, stairs, etc..) with optional filter parameters
 // @Accept  json
 // @Produce  json
-// @Tags MapController
+// @Tags MapControllermap
 // @Success 200 {array} models.MapItem
 // @Router /map [get]
 func (l MapController) GetMapItems(c *gin.Context) {
-	rooms, err := l.provider.GetAllRooms()
-	if err != nil {
-		fmt.Println("GetMapItems() failed with error", err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code":    http.StatusBadRequest,
-			"message": err.Error(),
-		})
+	floor := c.Query("floor")
+	name := c.Query("name") //mux.Vars(r)["name"]
+	alias := c.Query("alias")
+
+	//TODO include these filters
+	coordinate := c.Query("coordinate")
+	coordinateDelta := c.Query("coordinate-delta")
+	building := c.Query("building")
+	campus := c.Query("campus")
+	//----------------------------
+
+	var rooms []models.Room
+	var connectors []models.ConnectorSpace
+	var err error
+
+	var useFilterApi bool
+
+	if len(coordinate) == 0 && len(floor) == 0 && len(coordinateDelta) == 0 && len(building) == 0 && len(campus) == 0  {
+		//rooms, err = l.provider.GetAllRooms()
+		useFilterApi = false
+	} else {
+		useFilterApi = true
 	}
 
-	connectors, err := l.provider.GetAllConnectorSpaces()
+	if useFilterApi {
+		rooms, err = l.provider.FilterRooms(floor, name, "", "")
+	} else {
+		rooms, err = l.provider.GetAllRooms()
+	}
+
 	if err != nil {
 		fmt.Println("GetMapItems() failed with error", err)
 		c.JSON(http.StatusBadRequest, gin.H{
 			"code":    http.StatusBadRequest,
 			"message": err.Error(),
 		})
+
+		return
+	}
+
+	if useFilterApi {
+		connectors, err = l.provider.FilterConnectorSpaces(floor, name, alias, building, campus, nil, nil)
+	} else {
+		connectors, err = l.provider.GetAllConnectorSpaces()
+	}
+
+	if err != nil {
+		fmt.Println("GetMapItems() failed with error", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    http.StatusBadRequest,
+			"message": err.Error(),
+		})
+
+		return
 	}
 
 	l.CreateAndSendMapList(rooms, connectors, c)
