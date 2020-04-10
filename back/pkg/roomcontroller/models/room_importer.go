a3b20350bd68568730c397a16180cb3b456ddcb6
sharpSteff <sharp.kneidl@gmail.com>
2020-04-10 15:29:09 +0200

    working state

--- back/pkg/roomcontroller/models/room_importer.go
+++ back/pkg/roomcontroller/models/room_importer.go
@@ -6,6 +6,8 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
+	"studi-guide/ent"
+	"studi-guide/pkg/navigation"
 )
 
 type RoomImporter interface {
@@ -23,13 +25,19 @@ func (r *RoomJsonImporter) RunImport() error {
 		return err
 	}
 
-	var rooms []Room
-	err = json.NewDecoder(file).Decode(&rooms)
+	var items []ImportMapItems
+	err = json.NewDecoder(file).Decode(&items)
 	if err != nil {
 		return err
 	}
 
-	return r.dbService.AddRooms(rooms)
+	rooms, connectors, err2 :=  r.CreateMapItems(items);
+	if err2 != nil {
+		return err2
+	}
+
+	err = r.dbService.AddRooms(rooms)
+	return err
 }
 
 type RoomXmlImporter struct {
@@ -66,4 +74,56 @@ func NewRoomImporter(file string, dbService RoomServiceProvider) (RoomImporter, 
 	}
 
 	return i, nil
+}
+
+func (r *RoomJsonImporter) CreateMapItems (importItems []ImportMapItems ) ([]Room, error) {
+	var rooms []Room
+	for _, item := range importItems {
+			room := Room{
+				MapItem: MapItem{
+					Name:        item.Name,
+					Description: item.Description,
+					Tags:        item.Tags,
+					Color:       item.Color,
+					Floor:       item.Floor,
+					Sections:    item.Sections,
+					Campus:      item.Campus,
+					Building:    item.Building,
+				},
+				// Id should be set be DB
+				PathNodes: []*navigation.PathNode{
+					{
+					Id: item.PathNodes[0].Id,
+					Coordinate: navigation.Coordinate{
+						X: item.PathNodes[0].X,
+						Y: item.PathNodes[0].Y,
+						Z: item.PathNodes[0].Z,
+					},
+					Group: nil,
+				},
+				},
+			}
+			rooms = append(rooms, room)
+	}
+
+	return rooms, nil
+}
+
+func getPathNodesFromImport(importModes []ImportPathNode) []navigation.PathNode {
+	var nodes []navigation.PathNode
+	for _, item := range importModes {
+
+		nodes = append(nodes, navigation.PathNode{
+			Id:             item.Id,
+			Coordinate:    	navigation.Coordinate{
+				X: item.X,
+				Y: item.Y,
+				Z: item.Z,
+			},
+			Group:          nil,
+			ConnectedNodes: nil,
+		})
+	}
+
+	return nodes
 }
