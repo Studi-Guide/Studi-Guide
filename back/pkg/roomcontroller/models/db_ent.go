32665ade72744018014742cf9925dd569db89fd3
sharpSteff <sharp.kneidl@gmail.com>
2020-04-11 20:47:14 +0200

    remove obsolete lines

--- back/pkg/roomcontroller/models/db_ent.go
+++ back/pkg/roomcontroller/models/db_ent.go
@@ -546,15 +546,11 @@ func (r *RoomEntityService) linkPathNode(pathNode *navigation.PathNode) error {
 		connectedIDs = append(connectedIDs, entityConnectedNode.ID)
 	}
 
-	entityNode, err := r.client.PathNode.Get(r.context, pathNode.Id)
-	if err != nil {
-		return err
-	}
+	entityNode, _ := r.client.PathNode.Get(r.context, pathNode.Id)
 
 	update := entityNode.Update()
 	update.AddLinkedToIDs(connectedIDs...)
-	entityNode, err = update.Save(r.context)
-
+	entityNode, err := update.Save(r.context)
 	return err
 }
 
