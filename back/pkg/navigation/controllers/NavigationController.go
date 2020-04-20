ae8414d655bb03914d3a5c1999bc900ecb7e0d5e
sharpSteff <sharp.kneidl@gmail.com>
2020-04-20 23:46:48 +0200

    Fix navigation controller xml

--- back/pkg/navigation/controllers/NavigationController.go
+++ back/pkg/navigation/controllers/NavigationController.go
@@ -27,9 +27,9 @@ func NewNavigationController(router *gin.RouterGroup, service services.Navigatio
 // @Accept  json
 // @Produce  json
 // @Tags NavigationController
-// @Param startroom query string false "the start room name"
-// @Param endroom query string false "the end room name"
-// @Success 200 {array} navigation.NavigationRoute
+// @Param start query string false "the start location name"
+// @Param end query string false "the end location name"
+// @Success 200 navigation.NavigationRoute
 // @Router /navigation/dir [get]
 func (l *NavigationController) GetNavigationRoute(c *gin.Context) {
 	start := c.Query("start")
