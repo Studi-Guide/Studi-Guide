a29a88f336dccd75bb3c53276686d6b7d3788c92
s0m3-1 <34184055+s0m3-1@users.noreply.github.com>
2020-05-02 20:59:30 +0200

    added navigation.page.spec.ts as I moved it by mistake

---
+++ front/src/app/navigation/navigation.page.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { NavigationPage } from './navigationPage';
+
+describe('NavigationPage', () => {
+  let component: NavigationPage;
+  let fixture: ComponentFixture<NavigationPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [NavigationPage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NavigationPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
