97fa0e17702be5c3d3b4cfb16fa2c1a2f38184d4
s0m3-1 <34184055+s0m3-1@users.noreply.github.com>
2020-05-16 12:13:11 +0200

    Frontend shows error message after searching for a bad location

--- front/src/app/navigation/navigationPage.ts
+++ front/src/app/navigation/navigationPage.ts
@@ -74,4 +74,27 @@ export class NavigationPage {
       this.progressIsVisible = false;
     }
   }
+
+  private handleError(error: HttpErrorResponse) {
+
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      if (error.status === 400) {
+        this.progressIsVisible = false;
+        this.mapIsVisible = false;
+        //ToDo set this to false somewhere
+        this.errorOccured = true;
+      }
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+        'Something bad happened; please try again later.');
+  };
 }
