572a0b15a6ad4f884db648b2aa4a9aad8e6f4167
Semjon Wilhelm <semjon-wilhelm@hotmail.de>
2020-05-31 10:10:56 +0200

    Add search input component

---
+++ front/src/app/navigation/search-input/search-input.component.ts
@@ -0,0 +1,61 @@
+import {Component, Output, EventEmitter, OnInit, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.component.html',
+  styleUrls: ['./search-input.component.scss'],
+})
+export class SearchInputComponent implements OnInit {
+
+  @Output() search = new EventEmitter<string>();
+  @Output() route = new EventEmitter<string[]>();
+
+  @ViewChild('discoverySearchbar') discoverySearchbar;
+  @ViewChild('routeSearchBar') routeSearchBar;
+
+  private searchBtnIsVisible = true;
+  private routeInputIsVisible = false;
+  private closeRouteBtnIsVisible = false;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  private showRouteSearchBar() {
+      this.routeInputIsVisible = true;
+      const searchbars = document.querySelector('ion-item');
+      searchbars.setAttribute('color', 'primary');
+      this.searchBtnIsVisible = false;
+      this.closeRouteBtnIsVisible = true;
+  }
+
+  private hideRouteSearchBar() {
+    if (this.routeInputIsVisible) {
+      this.routeInputIsVisible = false;
+      const searchbars = document.querySelector('ion-item');
+      searchbars.setAttribute('color', 'light-tint');
+      this.searchBtnIsVisible = true;
+      this.closeRouteBtnIsVisible = false;
+    }
+  }
+
+  private routeBtnClick() {
+    if (!this.routeInputIsVisible) {
+      this.showRouteSearchBar();
+    } else {
+      this.emitRouteEvent();
+    }
+  }
+
+  private emitSearchEvent() {
+    this.search.emit(this.discoverySearchbar.value);
+    console.log('search event emitted', this.discoverySearchbar.value);
+  }
+
+  private emitRouteEvent() {
+    const route:string[] = [this.routeSearchBar.value, this.routeSearchBar.value];
+    this.route.emit(route);
+    console.log('route event emitted', this.discoverySearchbar.value, this.routeSearchBar.value);
+  }
+
+}
