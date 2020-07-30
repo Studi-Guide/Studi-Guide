f1925f9adc91b048cb6fdb0294081164c17ab2cd
Stefan Kneidl <sharp.kneidl@gmail.com>
2020-07-30 20:35:58 +0200

    Prototypische implementierung einer cross-tab navigation von Moodle-Schedule nach Location. Sobald man auf einen Locationnamen klickt, wird das Tab1 aufgemacht und nach dem Namen wird gesucht

--- front/src/app/navigation/navigation.module.ts
+++ front/src/app/navigation/navigation.module.ts
@@ -1,19 +1,23 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import {ActivatedRoute, RouterModule} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NavigationPage } from './navigationPage';
-import {SearchInputComponent} from "./search-input/search-input.component";
-import {MapViewComponent} from "./map-view/map-view.component";
+import {SearchInputComponent} from './search-input/search-input.component';
+import {MapViewComponent} from './map-view/map-view.component';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: NavigationPage }])
+    RouterModule.forChild([
+        { path: '', component: NavigationPage },
+        { path: 'search/:location', component: NavigationPage }
+        ])
   ],
     declarations: [NavigationPage, SearchInputComponent, MapViewComponent]
 })
-export class NavigationPageModule {}
+export class NavigationPageModule {
+}
