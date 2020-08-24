f3533db36c645a1bd9d111268b93c491d0461f62
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-08-24 20:57:26 +0200

    Add route to detail view component

--- front/src/app/tab4/tab4.module.ts
+++ front/src/app/tab4/tab4.module.ts
@@ -4,14 +4,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab4Page } from './tab4.page';
+import {IonicDetailViewModule} from "../../ionic-detail-view/ionic-detail-view.module";
+import {IonicDetailViewComponent} from "../../ionic-detail-view/ionic-detail-view.component";
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab4Page }])
-  ],
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([
+            {path: '', component: Tab4Page},
+            {path: 'language', component: IonicDetailViewComponent, }
+        ]),
+        IonicDetailViewModule
+    ],
   declarations: [Tab4Page]
 })
 export class Tab4PageModule {}
