30fbd3005656ea9b178a32cbec90dd5c7aaf3712
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-09-09 12:37:20 +0200

    Implement animation methods in map item renderer

--- front/src/app/navigation/map-view/map-item-renderer.canvas.ts
+++ front/src/app/navigation/map-view/map-item-renderer.canvas.ts
@@ -5,6 +5,9 @@ export class MapItemRendererCanvas implements IRenderer {
     constructor(private mapItem:IMapItem) {
     }
 
+    private doAnim = false;
+    private animationCallback: () => void;
+
     render(renderingContext: CanvasRenderingContext2D) {
         renderingContext.beginPath();
         renderingContext.moveTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
@@ -20,9 +23,33 @@ export class MapItemRendererCanvas implements IRenderer {
     }
 
     startAnimation(renderingContext: CanvasRenderingContext2D) {
+        this.animationCallback = () => {
+            if (this.doAnim !== true) {
+                return;
+            }
+            renderingContext.save();
+            const date = new Date();
+
+            renderingContext.beginPath();
+            renderingContext.moveTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
+            for (let i = 1; i < this.mapItem.Sections.length; i++) {
+                renderingContext.lineTo(this.mapItem.Sections[i].Start.X, this.mapItem.Sections[i].Start.Y);
+            }
+            renderingContext.lineTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
+            renderingContext.lineWidth = 1;
+            renderingContext.strokeStyle = (date.getSeconds() % 2 === 0) ? '#FFF' : '#000';
+            renderingContext.stroke();
+            renderingContext.closePath();
+
+            renderingContext.restore();
+
+            window.requestAnimationFrame(this.animationCallback);
+        }
     }
 
     stopAnimation(renderingContext: CanvasRenderingContext2D) {
+        this.doAnim = false;
+        this.animationCallback = null;
     }
 
 }
