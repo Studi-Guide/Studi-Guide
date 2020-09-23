0b4b686c4baf7c5716e98891c6045f187496c0c5
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-09-23 22:02:39 +0200

    Refactor duplicated code as suggested by sharpSteff

--- front/src/app/navigation/map-view/map-item-renderer.canvas.ts
+++ front/src/app/navigation/map-view/map-item-renderer.canvas.ts
@@ -14,11 +14,7 @@ export class MapItemRendererCanvas implements IRenderer {
 
     render(renderingContext: CanvasRenderingContext2D) {
         renderingContext.beginPath();
-        renderingContext.moveTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
-        for (let i = 1; i < this.mapItem.Sections.length; i++) {
-            renderingContext.lineTo(this.mapItem.Sections[i].Start.X,this. mapItem.Sections[i].Start.Y);
-        }
-        renderingContext.lineTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
+        this.renderSections(renderingContext);
         renderingContext.strokeStyle = '#FFF';
         renderingContext.fillStyle = this.mapItem.Color;
         renderingContext.stroke();
@@ -35,11 +31,7 @@ export class MapItemRendererCanvas implements IRenderer {
             const date = new Date();
 
             renderingContext.beginPath();
-            renderingContext.moveTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
-            for (let i = 1; i < this.mapItem.Sections.length; i++) {
-                renderingContext.lineTo(this.mapItem.Sections[i].Start.X, this.mapItem.Sections[i].Start.Y);
-            }
-            renderingContext.lineTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
+            this.renderSections(renderingContext);
             renderingContext.lineWidth = 1;
             renderingContext.strokeStyle = (date.getSeconds() % 2 === 0) ? '#FFF' : '#000';
             renderingContext.fillStyle = (date.getSeconds() % 2 === 0) ? '#000' : this.mapItem.Color;
@@ -66,6 +58,14 @@ export class MapItemRendererCanvas implements IRenderer {
     stopAnimation(renderingContext: CanvasRenderingContext2D) {
         this.doAnim = false;
         this.animationCallback = null;
+    }
+
+    private renderSections(renderingContext:CanvasRenderingContext2D) {
+        renderingContext.moveTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
+        for (let i = 1; i < this.mapItem.Sections.length; i++) {
+            renderingContext.lineTo(this.mapItem.Sections[i].Start.X, this.mapItem.Sections[i].Start.Y);
+        }
+        renderingContext.lineTo(this.mapItem.Sections[0].Start.X, this.mapItem.Sections[0].Start.Y);
     }
 
 }
