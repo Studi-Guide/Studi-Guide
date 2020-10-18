c722c77eda18e9ae0ce8666e034eed2fc9eec793
Stefan Kneidl <sharp.kneidl@gmail.com>
2020-10-18 23:32:02 +0200

    STDG-190 #comment JSON-Importer für Campus-Daten implementiert

---
+++ back/pkg/building/campus/campus_importer.go
@@ -0,0 +1,53 @@
+package campus
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/prometheus/common/log"
+	"os"
+	"path/filepath"
+	"studi-guide/pkg/building/db/ent"
+)
+
+type CampusImporter interface {
+	RunImport() error
+}
+
+type CampusJsonImporter struct {
+	dbService CampusProvider
+	file      string
+}
+
+func NewCampusImporter(file string, dbService CampusProvider) (CampusImporter, error) {
+	var i CampusImporter = nil
+	ext := filepath.Ext(file)
+	if ext == ".json" {
+		i = &CampusJsonImporter{dbService: dbService, file: file}
+	} else {
+		return nil, errors.New("Unknown extension")
+	}
+
+	return i, nil
+}
+
+func (r *CampusJsonImporter) RunImport() error {
+	file, err := os.Open(r.file)
+	if err != nil {
+		return err
+	}
+
+	var items []ent.Campus
+	err = json.NewDecoder(file).Decode(&items)
+	if err != nil {
+		return err
+	}
+
+	for _, campus := range items {
+		err = r.dbService.AddCampus(campus)
+		if err != nil {
+			log.Error(err)
+		}
+	}
+
+	return err
+}
--- back/pkg/building/db/entitymapper/campus_mapper.go
+++ back/pkg/building/db/entitymapper/campus_mapper.go
@@ -1,6 +1,7 @@
 package entitymapper
 
 import (
+	"github.com/prometheus/common/log"
 	"studi-guide/pkg/building/db/ent"
 	entcampus "studi-guide/pkg/building/db/ent/campus"
 )
@@ -39,4 +40,28 @@ func (r *EntityMapper) FilterCampus(name string) ([]*ent.Campus, error) {
 	}
 
 	return campus, nil
+}
+
+func (r *EntityMapper) AddCampus(campus ent.Campus) error {
+	found, _ := r.client.Campus.Query().Where(entcampus.NameEqualFold(campus.Name)).First(r.context)
+	if found != nil {
+		log.Info("campus ", campus.Name, " already imported")
+		return nil
+	}
+
+	_, err := r.client.Campus.
+		Create().
+		SetLongitude(campus.Longitude).
+		SetLatitude(campus.Latitude).
+		SetName(campus.Name).
+		SetShortName(campus.ShortName).
+		AddAddress(campus.Edges.Address...).
+		Save(r.context)
+
+	if err != nil {
+		log.Fatal("Error adding campus:", campus.Name, " Error:", err)
+		return err
+	}
+
+	return nil
 }
