2611e78b4e454184ff5b8864eb9810a159273ff2
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-10-18 18:24:36 +0200

    Use Renderer2 for animating bottom drawer

--- front/src/ionic-bottom-drawer/ionic-bottom-drawer.component.ts
+++ front/src/ionic-bottom-drawer/ionic-bottom-drawer.component.ts
@@ -4,12 +4,13 @@ import {
   ElementRef,
   EventEmitter,
   Input,
-  OnChanges, OnInit,
+  OnChanges,
+  OnInit,
   Output,
   Renderer2,
   SimpleChanges
 } from '@angular/core';
-import {AnimationController, DomController, Gesture, Animation, GestureController, Platform} from '@ionic/angular';
+import {Animation, AnimationController, DomController, Gesture, GestureController, Platform} from '@ionic/angular';
 import {DrawerState} from './drawer-state';
 
 @Component({
@@ -41,28 +42,20 @@ export class IonicBottomDrawerComponent implements OnInit, AfterViewInit, OnChan
 
   private startPositionTop: number;
   private gesture: Gesture;
-  private animation: Animation;
 
   constructor(
       private element: ElementRef,
       private renderer: Renderer2,
       private domCtrl: DomController,
       private platform: Platform,
-      private gestureCtrl: GestureController,
-      private animationCtrl: AnimationController
+      private gestureCtrl: GestureController
   ) { }
 
   ngOnInit() {
 
-    this.animation = this.animationCtrl.create()
-        .addElement(this.element.nativeElement)
-        .easing(this.easing)
-        .duration(5000)
-        .fromTo('transform', 'translateY(0px)'/*begin distance to top */, 'translateY('+this.platform.height()+'px)' /* end distance to top pos */);
-
     this.gesture = this.gestureCtrl.create({
       el: this.element.nativeElement,
-      threshold: 0,
+      threshold: 1,
       gestureName: 'push-pull-drawer',
       direction: 'y',
       onStart: (detail => {  this.onStart(detail); }),
@@ -70,14 +63,11 @@ export class IonicBottomDrawerComponent implements OnInit, AfterViewInit, OnChan
       onEnd: (detail => { this.onEnd(detail); })
     });
     this.gesture.enable();
+
+    this.renderer.setStyle(this.element.nativeElement, 'transition', 'transform 1ms ease-in-out');
   }
 
   ngAfterViewInit() {
-
-    //this.animation.progressStart(false);
-    //this.animation.progressStep(this.minimumHeight/this.platform.height());
-    //this.animation.progressEnd(1, 0, 5000);
-
     this.SetState(this.state);
   }
 
@@ -91,6 +81,10 @@ export class IonicBottomDrawerComponent implements OnInit, AfterViewInit, OnChan
 
   public async SetState(newState:DrawerState) {
     this.state = newState;
+
+    if (this.state !== DrawerState.Hidden) {
+      this.element.nativeElement.hidden = false;
+    }
 
     switch (this.state) {
        case DrawerState.Top:
@@ -107,87 +101,34 @@ export class IonicBottomDrawerComponent implements OnInit, AfterViewInit, OnChan
          break;
      }
 
+    if (this.state === DrawerState.Hidden) {
+      this.element.nativeElement.hidden = true;
+    }
+
      this.stateChange.emit(this.state);
   }
 
   private onStart(detail) {
-    this.startPositionTop = detail.startY//this.element.nativeElement.getBoundingClientRect().top;
+    this.startPositionTop = this.element.nativeElement.getBoundingClientRect().top;
     const step = (this.startPositionTop)/this.platform.height();
-
-    this.animation.progressStart(false, step);
     console.log(detail.startY, detail.currentY, detail.deltaY, step);
   }
 
   private onMove(detail) {
-    const step = (this.startPositionTop+detail.deltaY)/this.platform.height();
-    this.animation.progressStep(step);
-    console.log(detail.startY, detail.currentY, detail.deltaY, step);
+    const translate = 'translateY('+detail.currentY+'px)';
+    this.renderer.setStyle(this.element.nativeElement, 'transform', translate);
   }
 
   private onEnd(detail) {
-    //this.animation.to('transform', 'translateY('+(this.distanceTop)+'px)');
     const step = (this.startPositionTop+detail.deltaY)/this.platform.height();
-    this.animation.progressStep(step);
-    this.animation.pause();
-    //this.animation.progressEnd(0, 1);
     console.log(detail.startY, detail.currentY, detail.deltaY, step);
-    //this.animateTo(detail.currentY);
-    //this.animation.stop();  resets the animation
-
   }
 
   private async animateTo(positionY:number) {
     console.log(this.state, positionY, this.platform.height(), positionY/this.platform.height());
-    await this.animationCtrl.create()
-        .addElement(this.element.nativeElement)
-        .easing(this.easing)
-        .duration(this.duration)
-        .to('transform', 'translateY(' + (this.platform.height()-positionY) + 'px)')
-        .iterations(1)
-        .play();
+    const translate = 'translateY('+(this.platform.height()-positionY)+'px)';
+    this.renderer.setStyle(this.element.nativeElement, 'transform', translate);
+
   }
-
-  // private onMove(detail) {
-  //   const newTop = this.startPositionTop + detail.deltaY;
-  //
-  //   if ((this.shouldBounce && newTop < this.distanceTop - this.bounceDelta)
-  //       || (!this.shouldBounce && newTop < this.distanceTop)) {
-  //     return;
-  //   } else if ((this.shouldBounce && newTop > (this.platform.height() - this.minimumHeight) + this.bounceDelta)
-  //       || (!this.shouldBounce && newTop > (this.platform.height() - this.minimumHeight))) {
-  //     return;
-  //   }
-  //
-  //   this.animate(newTop);
-  // }
-
-  // private onStart(detail) {
-  //   this.startPositionTop = this.element.nativeElement.getBoundingClientRect().top;
-  // }
-  //
-  // private onEnd(detail) {
-  //   const newTop = this.startPositionTop + detail.deltaY;
-  //   const deltaTop = Math.abs(this.distanceTop - newTop);
-  //   const deltaDock = Math.abs(this.platform.height() - this.dockedHeight - newTop);
-  //   const deltaBot = Math.abs(this.platform.height() - this.minimumHeight - newTop);
-  //
-  //   if (deltaTop < deltaDock && deltaTop < deltaBot) {
-  //     this.SetState(DrawerState.Top);
-  //   } else if (deltaBot < deltaDock && deltaBot < deltaTop) {
-  //     this.SetState(DrawerState.Bottom);
-  //   } else {
-  //     this.SetState(DrawerState.Docked);
-  //   }
-  // }
-  //
-  // private async animate(position:number) {
-  //   await this.animationCtrl.create()
-  //       .addElement(this.element.nativeElement)
-  //       .easing(this.easing)
-  //       .duration(this.duration)
-  //       .to('transform', 'translateY(' + position + 'px)')
-  //       .iterations(1)
-  //       .play();
-  // }
 
 }
