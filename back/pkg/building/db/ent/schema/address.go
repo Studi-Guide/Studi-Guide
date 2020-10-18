ce69686b344d882d9ebd5cbaea15b49c50441d28
Stefan Kneidl <sharp.kneidl@gmail.com>
2020-10-18 22:10:52 +0200

    STDG-190 #comment Erweiterung der Campus-Tabelle PK(Street,Number,PLZ)

--- back/pkg/building/db/ent/schema/address.go
+++ back/pkg/building/db/ent/schema/address.go
@@ -4,6 +4,7 @@ import (
 	"github.com/facebook/ent"
 	"github.com/facebook/ent/schema/edge"
 	"github.com/facebook/ent/schema/field"
+	"github.com/facebook/ent/schema/index"
 )
 
 // Address holds the schema definition for the Address entity.
@@ -26,5 +27,13 @@ func (Address) Fields() []ent.Field {
 func (Address) Edges() []ent.Edge {
 	return []ent.Edge{
 		edge.From("campus", Campus.Type).Ref("address"),
+	}
+}
+
+func (Address) Indexes() []ent.Index {
+	return []ent.Index{
+		// unique index.
+		index.Fields("Street", "Number", "PLZ").
+			Unique(),
 	}
 }
