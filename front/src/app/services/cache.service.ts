a6736e24c37a87dfe344c761bfec8b2c18118e6e
Stefan Kneidl <sharp.kneidl@gmail.com>
2020-11-14 17:38:38 +0100

    STDG-237 #comment Cache-Service hat die option http erros nicht zu loggen

--- front/src/app/services/cache.service.ts
+++ front/src/app/services/cache.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {EMPTY, Observable} from 'rxjs';
 import {catchError, shareReplay} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
+import {log} from "util";
 
 @Injectable({
     providedIn: 'root'
@@ -10,7 +11,7 @@ export class CacheService {
 
     cache = {};
 
-    Get<T>(client: HttpClient,  request: string): Observable<T> {
+    Get<T>(client: HttpClient,  request: string, logOnError: boolean = true): Observable<T> {
         if (this.cache[request]) {
             return this.cache[request] as Observable<T>;
         }
@@ -19,7 +20,10 @@ export class CacheService {
         this.cache[request] = client.get<T>(request).pipe(
             shareReplay(1),
             catchError(err => {
-                console.log(err);
+                if (logOnError) {
+                    console.log(err);
+                }
+
                 delete this.cache[request];
                 return EMPTY;
             }));
