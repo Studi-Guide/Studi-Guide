d8fce21b2193f12f5426cb970b173590d7533b82
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-12-01 23:28:56 +0100

    Add GraphHopperRoute type

--- front/src/app/services/graph-hopper/graph-hopper.service.ts
+++ front/src/app/services/graph-hopper/graph-hopper.service.ts
@@ -4,6 +4,19 @@ import {Env} from '../../../environments/environment';
 import {LatLngLiteral} from 'leaflet';
 import {catchError} from "rxjs/operators";
 
+export interface GraphHopperPoints {
+  coordinates: [number, number][];
+  type: string;
+}
+
+export interface GraphHopperPath {
+  points: GraphHopperPoints;
+}
+
+export interface GraphHopperRoute {
+  paths: GraphHopperPath[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,11 +32,11 @@ export class GraphHopperService {
               private httpClient : HttpClient,
               private env : Env) { }
 
-  public async GetRouteEndpoint(startPoint:LatLngLiteral, endPoint:LatLngLiteral) {
+  public async GetRouteEndpoint(startPoint:LatLngLiteral, endPoint:LatLngLiteral) : Promise<GraphHopperRoute> {
     // curl "https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&vehicle=car&locale=de&calc_points=false&key=api_key"
     const r = await this.httpClient.get(this.ghRootUrl+this.ghRouteUrl
         +'?point='+startPoint.lat+','+startPoint.lng+'&point='+endPoint.lat+','+endPoint.lng
-        +'&vehicle='+this.ghVehicle+'&key='+this.ghApiKey+'&locale='+this.locale+'&points_encoded=false').toPromise();
+        +'&vehicle='+this.ghVehicle+'&key='+this.ghApiKey+'&locale='+this.locale+'&points_encoded=false').toPromise() as GraphHopperRoute;
     return r;
   }
 
