a2aeead100a7bb89eb1850bd7d021d7266536de0
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-12-06 13:06:24 +0100

    Add Graphhopper Key to Env

--- back/pkg/env/env.go
+++ back/pkg/env/env.go
@@ -8,28 +8,43 @@ import (
 
 type Env struct {
 	dbDriverName, dbDataSource, frontendPath string
+	graphHopperApiKey						 string
 	develop                                  bool
 }
 
 var dBDriverNameKey string = "DB_DRIVER_NAME"
 var dbDataSourceKey string = "DB_DATA_SOURCE"
 var frontendPath string = "FRONTEND_PATH"
+var graphHopperApiKey = "GRAPHHOPPER_API_KEY"
 var develop string = "DEVELOP"
-var gin_mode string = "GIN_MODE"
+
+var defaultDbDriverName = "sqlite3"
+var defaultDbDataSource = "db.sqlite3"
+var defaultFrontendPath = "./ionic"
 
 func NewEnv() *Env {
 
-	env := &Env{os.Getenv(dBDriverNameKey), os.Getenv(dbDataSourceKey), os.Getenv(frontendPath), false}
+	env := &Env{
+		dbDriverName: os.Getenv(dBDriverNameKey),
+		dbDataSource: os.Getenv(dbDataSourceKey),
+		frontendPath: os.Getenv(frontendPath),
+		graphHopperApiKey: os.Getenv(graphHopperApiKey),
+		develop: false,
+	}
 
 	if len(env.dbDriverName) == 0 && len(env.dbDataSource) == 0 {
 		log.Println("Using sqlite3 DB driver as no environment variables were provided.")
-		env.dbDriverName = "sqlite3"
-		env.dbDataSource = "db.sqlite3"
+		env.dbDriverName = defaultDbDriverName
+		env.dbDataSource = defaultDbDataSource
 	}
 
 	if len(env.frontendPath) == 0 {
-		log.Print("Using default frontend path ...")
-		env.frontendPath = "./ionic"
+		log.Println("Using default frontend path ...")
+		env.frontendPath = defaultFrontendPath
+	}
+
+	if len(env.graphHopperApiKey) == 0 {
+		log.Println("No Graphhopper API key provided. Openstreetmap route calculation will not be possible.")
 	}
 
 	if os.Getenv(develop) == "TRUE" {
@@ -46,7 +61,8 @@ func NewEnv() *Env {
 func (e Env) String() string {
 	return dBDriverNameKey + "=" + e.dbDriverName + ";" +
 		dbDataSourceKey + "=" + e.dbDataSource + ";" +
-		frontendPath + "=" + e.frontendPath
+		frontendPath + "=" + e.frontendPath + ";" +
+		graphHopperApiKey + "=" + e.graphHopperApiKey
 }
 
 func (e *Env) DbDriverName() string {
@@ -59,6 +75,10 @@ func (e *Env) DbDataSource() string {
 
 func (e *Env) FrontendPath() string {
 	return e.frontendPath
+}
+
+func (e *Env) GraphHopperApiKey() string {
+	return e.graphHopperApiKey
 }
 
 func (e *Env) Develop() bool {
---
+++ back/pkg/env/env_test.go
@@ -0,0 +1,80 @@
+package env
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_NewEnv_1(t *testing.T) {
+
+	dBDriverNameVar := "A"
+	dbDataSourceVar := "b"
+	frontendPathVar := "3"
+	graphHopperApiKeyVar := "D"
+	developVar := "-"
+
+	_ = os.Setenv(dBDriverNameKey, dBDriverNameVar)
+	_ = os.Setenv(dbDataSourceKey, dbDataSourceVar)
+	_ = os.Setenv(frontendPath, frontendPathVar)
+	_ = os.Setenv(graphHopperApiKey, graphHopperApiKeyVar)
+	_ = os.Setenv(develop, developVar)
+
+	env := NewEnv()
+
+	if env.DbDriverName() != dBDriverNameVar {
+		t.Error("env.dbDriverName", env.dbDriverName)
+	}
+
+	if env.DbDataSource() != dbDataSourceVar {
+		t.Error("env.dbDataSource", env.dbDataSource)
+	}
+
+	if env.FrontendPath() != frontendPathVar {
+		t.Error("env.frontendPath", env.frontendPath)
+	}
+
+	if env.GraphHopperApiKey() != graphHopperApiKeyVar {
+		t.Error("env.graphHopperApiKey", env.graphHopperApiKey)
+	}
+
+	if env.Develop() != false {
+		t.Error("env.develop", env.develop)
+	}
+
+}
+
+func Test_NewEnv_2(t *testing.T) {
+
+	graphHopperApiKeyVar := "???"
+	developVar := "TRUE"
+
+	// reset env variables
+	_ = os.Setenv(dBDriverNameKey, "")
+	_ = os.Setenv(dbDataSourceKey, "")
+	_ = os.Setenv(frontendPath, "")
+	_ = os.Setenv(graphHopperApiKey, graphHopperApiKeyVar)
+	_ = os.Setenv(develop, developVar)
+
+	env := NewEnv()
+
+	if env.DbDriverName() != defaultDbDriverName {
+		t.Error("env.dbDriverName", env.dbDriverName)
+	}
+
+	if env.DbDataSource() != defaultDbDataSource {
+		t.Error("env.dbDataSource", env.dbDataSource)
+	}
+
+	if env.FrontendPath() != defaultFrontendPath {
+		t.Error("env.frontendPath", env.frontendPath)
+	}
+
+	if env.GraphHopperApiKey() != graphHopperApiKeyVar {
+		t.Error("env.graphHopperApiKey", env.graphHopperApiKey)
+	}
+
+	if env.Develop() != true {
+		t.Error("env.develop", env.develop)
+	}
+
+}
