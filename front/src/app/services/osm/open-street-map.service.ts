256d3ca66e77f6e22bd0a727967ec6f25d0b8280
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-12-09 15:12:14 +0100

    Add locale to osm request

--- front/src/app/services/osm/open-street-map.service.ts
+++ front/src/app/services/osm/open-street-map.service.ts
@@ -55,7 +55,7 @@ export class OpenStreetMapService {
   public async GetRoute(startPoint:LatLngLiteral, endPoint:LatLngLiteral) : Promise<GraphHopperRoute> {
     // curl "https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&vehicle=car&locale=de&calc_points=false&key=api_key"
     const r = await this.httpClient.get(this.env.serverUrl+OpenStreetMapService.serverPathPrefix+OpenStreetMapService.routePath
-        +'?start='+startPoint.lat+','+startPoint.lng+'&end='+endPoint.lat+','+endPoint.lng).toPromise() as GraphHopperRoute;
+        +'?start='+startPoint.lat+','+startPoint.lng+'&end='+endPoint.lat+','+endPoint.lng+'&locale='+this.locale).toPromise() as GraphHopperRoute;
     return r;
   }
 
