d2926fe5f3837f44efa3165167133fa159bad0b0
NoUmlautsAllowed <semjon-wilhelm@hotmail.de>
2020-12-22 22:45:17 +0100

    Add navigation route input component

---
+++ front/src/app/navigation/nav-drawer-manager/route-input/route-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-input',
+  templateUrl: './route-input.component.html',
+  styleUrls: ['./route-input.component.scss'],
+})
+export class RouteInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
