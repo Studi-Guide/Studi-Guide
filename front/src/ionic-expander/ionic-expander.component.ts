bea6df89fa2a5cda42284177b064a06f8799de91
sharpSteff <sharp.kneidl@gmail.com>
2021-01-03 15:07:47 +0100

    Fix expanded height and styling

--- front/src/ionic-expander/ionic-expander.component.ts
+++ front/src/ionic-expander/ionic-expander.component.ts
@@ -1,22 +1,37 @@
-import {AfterViewInit, Component, ElementRef, Input, Renderer2, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  Renderer2,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-expander',
   templateUrl: './ionic-expander.component.html',
   styleUrls: ['./ionic-expander.component.scss'],
 })
-export class IonicExpanderComponent implements AfterViewInit {
+export class IonicExpanderComponent implements OnChanges {
   @ViewChild('expandWrapper', { read: ElementRef }) expandWrapper: ElementRef;
+  @ViewChild('expandContent', { read: ElementRef }) expandContent: ElementRef;
   @Input() expanded = false;
-  @Input() expandHeight = '150px';
   @Input() header = '';
-  constructor(public renderer: Renderer2) {}
-
-  ngAfterViewInit() {
-    this.renderer.setStyle(this.expandWrapper.nativeElement, 'max-height', this.expandHeight);
+  constructor(public renderer: Renderer2) {
   }
 
-    expand() {
-        this.expanded = !this.expanded;
+  expand() {
+      this.expanded = !this.expanded;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.expanded.currentValue &&
+        (changes.expanded.currentValue !== changes.expanded.previousValue)) {
+      const rect = (this.expandContent.nativeElement as HTMLDivElement).getBoundingClientRect();
+      this.renderer.setStyle(this.expandWrapper.nativeElement, 'max-height', rect.height);
+      this.renderer.setStyle(this.expandWrapper.nativeElement, 'transition', 'max-height 0.4s ease-in-out');
     }
+  }
 }
