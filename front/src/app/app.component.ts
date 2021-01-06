033df008035a03bab5abd7592692767ffc20967c
sharpSteff <sharp.kneidl@gmail.com>
2021-01-06 09:15:30 +0100

    STDG-299 #comment Automatische erkennung von Dark-Mode auf endgeräten aktiviert (funktioniert auf macOS und iOS)

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
     private platform: Platform,
     private settingsModel: SettingsModel /* inject this to initialize global settings on startup */
   ) {
+    this.initializeDarkMode();
     this.initializeApp();
   }
 
@@ -25,5 +26,19 @@ export class AppComponent {
             .then(() => SplashScreen.hide().then());
       });
     }
+  }
+
+  private initializeDarkMode() {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    this.toggleDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addListener((mediaQuery) => this.toggleDarkTheme(mediaQuery.matches));
+  }
+
+  toggleDarkTheme(shouldAdd) {
+    document.body.classList.toggle('dark', shouldAdd);
   }
 }
