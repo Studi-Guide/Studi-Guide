6d1d0d1cd04ede6d4749aaa7fdaa64160ef2c1e1
sharpSteff <30927510+sharpSteff@users.noreply.github.com>
2021-02-14 23:05:06 +0100

    Update generate.go

--- back/pkg/rssfeed/generate.go
+++ back/pkg/rssfeed/generate.go
@@ -1,3 +1,3 @@
 package rssfeed
 
-//go:generate go run github.com/golang/mock/mockgen -package=rssFeed -source=provider.go -destination=mock_provider.go
+//go:generate go run github.com/golang/mock/mockgen -package=rssfeed -source=provider.go -destination=mock_provider.go
