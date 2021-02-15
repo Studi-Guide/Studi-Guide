519dba5f4aa8f55660c3c92aeb060572624421d8
sharpSteff <sharp.kneidl@gmail.com>
2021-02-15 23:07:48 +0100

    cleanup

--- front/src/app/navigation/map-view/renderer-provider.ts
+++ front/src/app/navigation/map-view/renderer-provider.ts
@@ -6,24 +6,24 @@ import {RouteRendererCanvas} from './route-renderer.canvas';
 
 
 export class RendererProvider {
-    public static GetMapItemRendererCanvas(...mapItem:IMapItem[]) : MapItemRendererCanvas[] {
-        const renderer:MapItemRendererCanvas[] = [];
+    public static GetMapItemRendererCanvas(...mapItem: IMapItem[]): MapItemRendererCanvas[] {
+        const renderer: MapItemRendererCanvas[] = [];
         for (const m of mapItem) {
             renderer.push(new MapItemRendererCanvas(m));
         }
         return renderer;
     }
 
-    public static GetLocationRendererCanvas(...location:ILocation[]) : LocationRendererCanvas[] {
-        const renderer:LocationRendererCanvas[] = [];
+    public static GetLocationRendererCanvas(...location: ILocation[]): LocationRendererCanvas[] {
+        const renderer: LocationRendererCanvas[] = [];
         for (const l of location) {
             renderer.push(new LocationRendererCanvas(l));
         }
         return renderer;
     }
 
-    public static GetRouteRendererCanvas(...route:IReceivedRoute[]) : RouteRendererCanvas[] {
-        const renderer:RouteRendererCanvas[] = [];
+    public static GetRouteRendererCanvas(...route: IReceivedRoute[]): RouteRendererCanvas[] {
+        const renderer: RouteRendererCanvas[] = [];
         for (const r of route) {
             renderer.push(new RouteRendererCanvas(r));
         }
