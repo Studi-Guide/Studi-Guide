4944a56faa0e906bf3451d99d42111090c27abbb
sharpSteff <sharp.kneidl@gmail.com>
2021-02-25 13:06:27 +0100

    update search service

---
+++ back/cmd/studi-guide-search/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	searchnservice "studi-guide/cmd/studi-guide-search/server"
+	"studi-guide/docs"
+	"studi-guide/pkg/building/campus"
+	"studi-guide/pkg/building/db/entitymapper"
+	"studi-guide/pkg/building/info"
+	"studi-guide/pkg/building/location"
+	maps "studi-guide/pkg/building/map"
+	"studi-guide/pkg/building/room/models"
+	"studi-guide/pkg/env"
+	"studi-guide/pkg/navigation/services"
+	"studi-guide/pkg/utils"
+
+	"go.uber.org/dig"
+)
+
+func main() {
+
+	log.Print("Starting service")
+
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "StuidGuide API"
+	docs.SwaggerInfo.Description = "This is the search service of studiGuide."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
+	container := BuildContainer()
+
+	error := container.Invoke(func(server *searchnservice.SearchMicroService) {
+		port := ":8080"
+		log.Printf("Starting http listener on %s", port)
+		log.Fatal(server.Start(port))
+	})
+
+	log.Fatal(error)
+}
+
+func defaultLogger() *log.Logger {
+	return log.New(os.Stdout, log.Prefix(), log.Flags())
+}
+
+func BuildContainer() *dig.Container {
+
+	container := dig.New()
+
+	container.Provide(func() utils.HttpClient {
+		return http.DefaultClient
+	})
+
+	container.Provide(env.NewEnv)
+	container.Provide(defaultLogger)
+	container.Provide(entitymapper.NewEntityMapper)
+
+	// Register entity service for multiple interfaces
+	container.Invoke(func(entityserver *entitymapper.EntityMapper) {
+		container.Provide(func() services.PathNodeProvider {
+			return entityserver
+		})
+
+		container.Provide(func() models.RoomServiceProvider {
+			return entityserver
+		})
+
+		container.Provide(func() location.LocationProvider {
+			return entityserver
+		})
+
+		container.Provide(func() maps.MapServiceProvider {
+			return entityserver
+		})
+
+		container.Provide(func() info.BuildingProvider {
+			return entityserver
+		})
+
+		container.Provide(func() campus.CampusProvider {
+			return entityserver
+		})
+	})
+
+	container.Provide(searchnservice.NewSearchService)
+
+	// Register entity service for multiple interfaces
+	container.Invoke(func(entityserver *entitymapper.EntityMapper) {
+		container.Provide(func() services.PathNodeProvider {
+			return entityserver
+		})
+	})
+
+	return container
+}
---
+++ back/cmd/studi-guide-search/server/studiguide_search_server.go
@@ -0,0 +1,117 @@
+package searchnservice
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+	"log"
+	"net/http"
+	"studi-guide/pkg/building/campus"
+	buildingInfo "studi-guide/pkg/building/info"
+	buildingLocation "studi-guide/pkg/building/location"
+	buildingMap "studi-guide/pkg/building/map"
+	"studi-guide/pkg/building/room/controllers"
+	buildingRoom "studi-guide/pkg/building/room/models"
+	"studi-guide/pkg/env"
+)
+
+type SearchMicroService struct {
+	router *gin.Engine
+}
+
+func NewSearchService(env *env.Env,
+	buildingProvider buildingInfo.BuildingProvider, roomProvider buildingRoom.RoomServiceProvider,
+	locationProvider buildingLocation.LocationProvider, mapProvider buildingMap.MapServiceProvider,
+	campusProvider campus.CampusProvider) *SearchMicroService {
+	log.Print("Starting initializing main controllers ...")
+	router := gin.Default()
+
+	if env.Develop() {
+		log.Println("allowing all origins in develop mode")
+		router.Use(cors.New(cors.Config{
+			AllowAllOrigins: true,
+		}))
+	}
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	router.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(gin.Recovery())
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	//router.GET("/", RedirectRootToAPI(router))
+	//router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+
+	roomRouter := router.Group("/rooms")
+	{
+		log.Print("Creating room controllers")
+		err := controllers.NewRoomController(roomRouter, roomProvider)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Print("Successfully initialized room controllers")
+		}
+		//a.Run(":8080")
+	}
+
+	locationRouter := router.Group("/locations")
+	{
+		log.Println("Creating location controller")
+		err := buildingLocation.NewLocationController(locationRouter, locationProvider)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Println("Successfully initialized location controller")
+		}
+	}
+
+	buildingRouter := router.Group("/buildings")
+	{
+		log.Println("Creating building controller")
+		err := buildingInfo.NewBuildingController(buildingRouter, buildingProvider, roomProvider, locationProvider, mapProvider)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Println("Successfully initialized building controller")
+		}
+	}
+
+	campusRouter := router.Group("/campus")
+	{
+		log.Println("Creating campus controller")
+		err := campus.NewCampusController(campusRouter, campusProvider)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Println("Successfully initialized campus controller")
+		}
+	}
+
+	server := SearchMicroService{router: router}
+	return &server
+}
+
+func (server *SearchMicroService) Start(port string) error {
+	err := http.ListenAndServe(port, server.router)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if len(authHeader) == 0 {
+			// Authorization example
+			// httputil.NewError(c, http.StatusUnauthorized, errors.New("Authorization is required Header"))
+			//c.Abort()
+		}
+
+		c.Next()
+	}
+}
