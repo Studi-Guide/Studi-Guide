dae93c726c288630c50fe034c87af31619f11063
sharpSteff <sharp.kneidl@gmail.com>
2021-02-26 13:55:29 +0100

    remove obsolete start searchbar

--- front/src/app/navigation/search-input/search-input.component.ts
+++ front/src/app/navigation/search-input/search-input.component.ts
@@ -19,10 +19,8 @@ export class SearchInputComponent implements OnInit {
   @Output() searchBarFocus = new EventEmitter<string>();
 
   @ViewChild('destinationSearchbar') destinationSearchbar: HTMLIonSearchbarElement;
-  @ViewChild('startSearchBar') startSearchBar: HTMLIonSearchbarElement;
 
   public searchBtnIsVisible = true;
-  public routeInputIsVisible = false;
   public closeRouteBtnIsVisible = false;
   public startSearchBarValue: string;
   public destinationSearchbarValue: string;
@@ -68,32 +66,6 @@ export class SearchInputComponent implements OnInit {
     }
   }
 
-  public showRouteSearchBar() {
-      this.routeInputIsVisible = true;
-      // const searchbars = document.querySelector('ion-item');
-      // searchbars.setAttribute('color', 'primary');
-      this.searchBtnIsVisible = false;
-      this.closeRouteBtnIsVisible = true;
-  }
-
-  public hideRouteSearchBar() {
-    if (this.routeInputIsVisible) {
-      this.routeInputIsVisible = false;
-      // const searchbars = document.querySelector('ion-item');
-      // searchbars.setAttribute('color', 'light-tint');
-      this.searchBtnIsVisible = true;
-      this.closeRouteBtnIsVisible = false;
-    }
-  }
-
-  public routeBtnClick() {
-    if (!this.routeInputIsVisible) {
-      this.showRouteSearchBar();
-    } else {
-      this.emitRouteEvent();
-    }
-  }
-
   public emitDiscoveryEvent() {
     let isInputEmpty = this.destinationSearchbar.value === '' || this.destinationSearchbar.value === undefined;
     isInputEmpty = isInputEmpty || this.destinationSearchbar.value === null;
@@ -105,24 +77,6 @@ export class SearchInputComponent implements OnInit {
       }
 
       this.discovery.emit(this.destinationSearchbar.value);
-    }
-  }
-
-  public emitRouteEvent() {
-    let isStartEmpty = this.destinationSearchbar.value === '' || this.destinationSearchbar.value === undefined;
-    isStartEmpty = isStartEmpty || this.destinationSearchbar.value === null;
-    let isDestinationEmpty = this.startSearchBar.value === '' || this.startSearchBar.value === undefined;
-    isDestinationEmpty = isDestinationEmpty || this.startSearchBar.value === null;
-    if (!isStartEmpty && !isDestinationEmpty) {
-      const route: string[] = [this.startSearchBar.value, this.destinationSearchbar.value];
-
-      // Workaround for https://github.com/ionic-team/ionic-v3/issues/217
-      const activeElement = document.activeElement as HTMLElement;
-      if (activeElement && activeElement.blur){
-        activeElement.blur();
-      }
-
-      this.route.emit(route);
     }
   }
 
@@ -151,10 +105,6 @@ export class SearchInputComponent implements OnInit {
     if (e.key === 'Enter') {
       if (inputElement === 'destinationSearchbar') {
           this.emitDiscoveryEvent();
-      }
-
-      if (inputElement === 'startSearchBar') {
-          this.emitRouteEvent();
       }
     }
   }
